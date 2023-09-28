93b33baab3fb6740d4f612ff76431b5a427d1a0e
Vinicius de Camargo Fantin <415238@unoesc.lan>
2023-09-27 22:05:41 -0300

    quarto commit

---
+++ Atividades/Trees/Tree.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+namespace Trees
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+    }
+}
